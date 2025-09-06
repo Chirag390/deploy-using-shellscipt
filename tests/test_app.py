a72bde8ed016033d10b46991ff70b0bc82eeac33
chirag goyal <chiragac2125@gmail.com>
2025-09-06 13:28:41 +0000

    this is dummy portfolio website deployed though shell scripting automatically every time the code changes

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,15 @@
 from app import app
 
+
 def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
     assert b"My Portfolio" in response.data
 
+
 def test_projects():
     client = app.test_client()
     response = client.get("/projects")
     assert response.status_code == 200
     assert b"Projects" in response.data
-
